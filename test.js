3d23ac29fc93fba555e3c83f49f5fca2ca2b5491
mufassir-mahmud <mufassirmahmud01821@gmail.com>
2023-10-24 01:06:10 +0600

    secon commit

--- test.js
+++ test.js
@@ -1,2 +1,22 @@
+let flowerTotal = 0;
 function handleClick(target){
+    const selectedITem = document.getElementById('selected-item');
+    const itemName = target.parentNode.parentNode.childNodes[1].innerText;
     
+    const li = document.createElement('li');
+    li.innerText = itemName;
+   
+    selectedITem.appendChild(li)
+    const price = target.parentNode.parentNode.childNodes[5].childNodes[1].innerText;
+    
+    
+    flowerTotal = parseInt(flowerTotal) + parseInt(price);
+    console.log(flowerTotal);
+
+    const findTotal = document.getElementById('total');
+    findTotal.innerText = flowerTotal;
+
+    if(flowerTotal === 200){
+        
+    }
+}
